3e8f2119e7695d86dbc144db4067b7f49a51d101
Andreus <Andreus@DESKTOP-I3EQQ7H>
2020-06-19 11:50:41 +0500

    Добавьте файлы проекта.

---
+++ ООП-1.2/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ООП_1._2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public class Line
+        {
+            private string textLine;
+            public void textFill(string text)
+            {
+                if (text.Length > 10) textLine = "Слишком длинная строка";
+                else textLine = text;
+            }
+            public void textFill()
+            {
+                textLine = "Пусто";
+            }
+        }
+    }
+}
